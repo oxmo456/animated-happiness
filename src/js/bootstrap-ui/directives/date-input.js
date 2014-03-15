5194c24e04b845acad61a477a74449b131195e1f
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-03-15 19:10:46 -0400

    ...

--- src/js/bootstrap-ui/directives/date-input.js
+++ src/js/bootstrap-ui/directives/date-input.js
@@ -1,14 +1,35 @@
-angular.module("bootstrap-ui").directive("date", function () {
+angular.module("bootstrap-ui").directive("dateInput", function () {
+
+
+    function extractYears(k) {
+        var result = [];
+        var year = (new Date()).getFullYear();
+        while (k--) {
+            result.push({label: year, value: year});
+            year--;
+        }
+        return result;
+    }
+
+    function extractMonths() {
+        var result = [];
+        var year = (new Date()).getFullYear();
+        while (k--) {
+            result.push({label: year, value: year});
+            year--;
+        }
+        return result;
+    }
 
     return {
         restrict: "A",
-        templateUrl: "/templates/bootstrap-ui/directives/date.template.html",
+        templateUrl: "/templates/bootstrap-ui/directives/date-input.template.html",
         scope: {
-            date: "="
+            date: "=dateInput"
         },
         link: function (scope, element, attribute) {
-
-
+            scope.years = extractYears(10);
+            scope.months = extractMonths();
         }
     };
 
