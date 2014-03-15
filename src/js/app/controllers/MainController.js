6bc504e89569b875ecf78dc9c9dd270fff1a0ea9
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-03-15 18:09:24 -0400

    ...

--- src/js/app/controllers/MainController.js
+++ src/js/app/controllers/MainController.js
@@ -1,5 +1,14 @@
 angular.module("app").controller("MainController", function ($scope, StickiesStorage) {
 
+    var STICKY_NOTES_THEMES = [
+        {name: "Post-It", id: "post-it"},
+        {name: "Blue", id: "blue"},
+        {name: "Red", id: "red"},
+        {name: "Green", id: "green"},
+        {name: "Black", id: "black"},
+    ];
+
+    var DEFAULT_THEME_ID = STICKY_NOTES_THEMES[0].id;
 
     function saveStickies(stickies) {
         StickiesStorage.save(stickies);
@@ -22,7 +31,7 @@ angular.module("app").controller("MainController", function ($scope, StickiesSto
     $scope.addStickyNote = function () {
         $scope.stickies.push({
             text: "",
-            color: "",
+            themeId: DEFAULT_THEME_ID,
             position: {
                 x: 0,
                 y: 0
@@ -31,12 +40,17 @@ angular.module("app").controller("MainController", function ($scope, StickiesSto
     };
 
     $scope.editStickyNoteMeta = function (stickyNote) {
-        $scope.modal.show({stickyNote: stickyNote}, "editStickyNoteModal");
+        $scope.modal.show({
+            stickyNote: stickyNote,
+            themes: STICKY_NOTES_THEMES
+        }, "editStickyNoteModal");
     };
 
     $scope.deleteStickyNote = function (stickyNote) {
         console.log("delete", stickyNote);
     };
+
+    $scope.colors = STICKY_NOTES_THEMES;
 
     initialize();
 
