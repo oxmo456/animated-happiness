06c6821b6f38b78b5928fdf11d310c82395518e4
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-03-15 18:50:20 -0400

    ...

--- src/js/app/controllers/MainController.js
+++ src/js/app/controllers/MainController.js
@@ -32,6 +32,7 @@ angular.module("app").controller("MainController", function ($scope, StickiesSto
         $scope.stickies.push({
             text: "",
             themeId: DEFAULT_THEME_ID,
+            date: new Date(),
             position: {
                 x: 0,
                 y: 0
